af473b7a00560c3305726ff52678ab2cee7f2edd
wommy <tom@technomad.media>
2022-03-31 07:35:22 -0400

    yolo: vite not working without config hacking

--- .eleventy.js
+++ .eleventy.js
@@ -5,16 +5,20 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("img")
+    .addPassthroughCopy("css")
+    .addPassthroughCopy({ public: "/" }); // w0m
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  // w0m
+  eleventyConfig.addPlugin(EleventyVitePlugin);
 
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
