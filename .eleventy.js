252601ff2e22cff71fd10e80d74f7bf244594305
wommy <tom@technomad.media>
2022-04-03 07:18:11 -0400

    src dir

--- .eleventy.js
+++ .eleventy.js
@@ -123,10 +123,7 @@ module.exports = function (eleventyConfig) {
 
     // These are all optional (defaults are shown):
     dir: {
-      input: '.',
-      includes: '_includes',
-      data: '_data',
-      output: '_site',
+      input: 'src',
     },
   }
 }
