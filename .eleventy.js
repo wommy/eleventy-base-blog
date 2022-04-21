b832f3f40c80f8e1c434eef438718144daf32850
wommy <tom@technomad.media>
2022-04-20 20:55:20 -0400

    CLEAN: comments

--- .eleventy.js
+++ .eleventy.js
@@ -81,33 +81,10 @@ module.exports = function (eleventyConfig) {
     .use(MDITtaskLists, { enabled: true })
   eleventyConfig.setLibrary('md', markdownLibrary)
 
-  // Override Browsersync defaults (used only with --serve)
-  // eleventyConfig.setBrowserSyncConfig({
-  //   callbacks: {
-  //     ready: function (err, browserSync) {
-  //       const content_404 = fs.readFileSync('_site/404.html')
-
-  //       browserSync.addMiddleware('*', (req, res) => {
-  //         // Provides the 404 content without redirect.
-  //         res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
-  //         res.write(content_404)
-  //         res.end()
-  //       })
-  //     },
-  //   },
-  //   ui: false,
-  //   ghostMode: false,
-  // })
 
   return {
-    // Control which files Eleventy will process
-    // e.g.: *.md, *.njk, *.html, *.liquid
     templateFormats: ['md', 'njk', 'html', 'liquid'],
-
-    // Pre-process *.md files with: (default: `liquid`)
     markdownTemplateEngine: 'njk',
-
-    // Pre-process *.html files with: (default: `liquid`)
     htmlTemplateEngine: 'njk',
 
     // -----------------------------------------------------------------
